d591df6c07a007271acf7b3e2445e05f141b2ef3
seono9y <144989211+seono9y@users.noreply.github.com>
2023-10-19 16:24:54 +0900

    06-1

---
+++ exercise_07/exercise_07/sub.c
@@ -0,0 +1,14 @@
+//
+//  sub.c
+//  exercise_07
+//
+//  Created by 추서연 on 2023/10/19.
+//
+
+#include "sub.h"
+
+extern int all_files;
+void sub(void)
+{
+all_files = 10;
+}
---
+++ exercise_07/exercise_07/sub.h
@@ -0,0 +1,13 @@
+//
+//  sub.h
+//  exercise_07
+//
+//  Created by 추서연 on 2023/10/19.
+//
+
+#ifndef sub_h
+#define sub_h
+
+#include <stdio.h>
+
+#endif /* sub_h */
