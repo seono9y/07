a5595a088bd35d00678d4d1fc32b8a18640b80f8
seono9y <144989211+seono9y@users.noreply.github.com>
2023-10-19 15:30:00 +0900

    02

--- exercise_07/exercise_07/main.c
+++ exercise_07/exercise_07/main.c
@@ -7,16 +7,15 @@
 
 #include <stdio.h>
 
-    int main(void)
-    {
-    int flag = 1;
-    while(flag != 0)
-    {
-    int y;
-    y = 3;
-    flag = 0;
-    }
-    int y;
-    y=4;
-        return 0;
-    }
+int main(void)
+{
+    int i;
+    int temp = 1;
+    
+    for (i=0;i<5;i++)
+{
+    printf("temp = %d\n", temp);
+    temp++;
+}
+    return 0;
+}
