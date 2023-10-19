fd857d348b6627790a28a6052f78c370ca1db486
seono9y <144989211+seono9y@users.noreply.github.com>
2023-10-19 15:19:29 +0900

    01

---
+++ exercise_07/exercise_07/main.c
@@ -0,0 +1,22 @@
+//
+//  main.c
+//  exercise_07
+//
+//  Created by 추서연 on 2023/10/19.
+//
+
+#include <stdio.h>
+
+    int main(void)
+    {
+    int flag = 1;
+    while(flag != 0)
+    {
+    int y;
+    y = 3;
+    flag = 0;
+    }
+    int y;
+    y=4;
+        return 0;
+    }
